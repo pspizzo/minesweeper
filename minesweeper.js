1d78dbb769cd6f7166ca988eaf30c87962ce3b67
Patrick Spizzo <pspizzo@gmail.com>
2025-11-04 11:51:30 -0600

    Detect game win condtion and show game status icon; reset game when clicking game status icon

--- minesweeper.js
+++ minesweeper.js
@@ -3,6 +3,7 @@ let size = 0;
 let totalMines = 0;
 
 let cellsFlagged = 0;
+let cellsRevealed = 0;
 
 // Data representation of board.
 // Each cell shows the number of neighboring mines, 0 through 8.
@@ -10,6 +11,7 @@ let cellsFlagged = 0;
 let board = [];
 
 let gameRunning = false;
+let win = false;
 let lose = false;
 
 const explosionSvg = '<svg class="explosion" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="m480-281 59-59h81v-81l59-59-59-59v-81h-81l-59-59-59 59h-81v81l-59 59 59 59v81h81l59 59Zm0 253L346-160H160v-186L28-480l132-134v-186h186l134-132 134 132h186v186l132 134-132 134v186H614L480-28Zm0-112 100-100h140v-140l100-100-100-100v-140H580L480-820 380-720H240v140L140-480l100 100v140h140l100 100Zm0-340Z"/></svg>';
@@ -22,6 +24,25 @@ function getCellElement(row, cell) {
 function updateMineCountDisplay() {
     const counter = document.getElementById('mines-remaining');
     counter.innerText = String(totalMines - cellsFlagged);
+}
+
+function updateStatusIcon() {
+    const statusEl = document.getElementById('status-icon');
+    statusEl.classList.remove('neutral');
+    statusEl.classList.remove('happy');
+    statusEl.classList.remove('sad');
+    if (!gameRunning) {
+        statusEl.classList.add('neutral');
+        return;
+    }
+
+    if (lose) {
+        statusEl.classList.add('sad');
+    } else if (win) {
+        statusEl.classList.add('happy');
+    } else {
+        statusEl.classList.add('neutral');
+    }
 }
 
 function rightClickHandler(e) {
@@ -63,13 +84,23 @@ function cellClickHandler(e) {
         lose = true;
         document.getElementById('game-board').classList.add('lose');
         e.target.innerHTML = explosionSvg;
+        updateStatusIcon();
         gameRunning = false;
     } else if (board[row][cell] === 0) {
         e.target.setAttribute('data-state', 'visible');
+        cellsRevealed += 1;
     } else {
         e.target.setAttribute('data-state', 'visible');
         e.target.innerHTML = board[row][cell];
         e.target.setAttribute('data-neighboring-mines', board[row][cell]);
+        cellsRevealed += 1;
+    }
+
+    if (cellsRevealed === ((size * size) - totalMines)) {
+        win = true;
+        document.getElementById('game-board').classList.add('win');
+        updateStatusIcon();
+        gameRunning = false;
     }
 }
 
@@ -147,15 +178,26 @@ function initGame(numRowsAndCols, numMines) {
     }
 
     const gameBoardEl = document.getElementById('game-board');
+    gameBoardEl.classList.remove('win');
     gameBoardEl.classList.remove('lose');
 
+    cellsFlagged = 0;
+    cellsRevealed = 0;
+    win = false;
     lose = false;
 
     totalMines = numMines;
     layMines();
+    updateStatusIcon();
 
     gameRunning = true;
 }
 
-initGame(9, 10);
+document.addEventListener('DOMContentLoaded', () => {
+    initGame(9, 10);
+})
+
+document.getElementById('status-icon').addEventListener('click', () => {
+    initGame(9, 10);
+});
 
